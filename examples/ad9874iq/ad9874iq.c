63eef9dfbeee8c38a7ccbcc0e76c97b2e07a88c3
Kaz Kojima <kkojima@rr.iij4u.or.jp>
2022-06-18 14:50:33 +0900

    Add some weak magics.

--- examples/ad9874iq/ad9874iq.c
+++ examples/ad9874iq/ad9874iq.c
@@ -5,9 +5,36 @@
 extern int printf(const char *format, ...);
 extern int puts(const char *s);
 extern char uart_read(void);
-extern void udp_start_constprop_0(unsigned int ip, const unsigned char *macaddr);
-extern int udp_send_constprop_0_isra_0(unsigned short dst_port, unsigned int length);
 extern int udp_arp_resolve(unsigned int ip);
+
+extern void __attribute__((weak)) udp_start(const unsigned char *macaddr, unsigned int ip);
+extern int __attribute__((weak)) udp_send(unsigned short src_port, unsigned short dst_port, unsigned int length);
+extern void __attribute__((weak)) udp_start_constprop_0(unsigned int ip, const unsigned char *macaddr);
+extern int __attribute__((weak)) udp_send_constprop_0_isra_0(unsigned short dst_port, unsigned int length);
+void * __attribute__((weak)) udp_get_tx_buffer(void);
+
+void xudp_start(const unsigned char *macaddr, unsigned int ip)
+{
+  if (udp_start)
+    udp_start(macaddr, ip);
+  else if (udp_start_constprop_0)
+    udp_start_constprop_0(ip, macaddr);
+  else
+    printf("no udp_start variants\n");
+}
+
+int xudp_send(unsigned short src_port, unsigned short dst_port, unsigned int length)
+{
+  if (udp_send)
+    return udp_send(src_port, dst_port, length);
+  else if (udp_send_constprop_0_isra_0)
+    // src_port ignored
+    return udp_send_constprop_0_isra_0(dst_port, length);
+  else {
+    printf("no udp_send variants\n");
+    return -1;
+  }
+}
 
 static unsigned char macadr[6] = {0x10, 0xe2, 0xd5, 0x00, 0x00, 0x00};
 
@@ -15,8 +42,11 @@ static unsigned char macadr[6] = {0x10, 0xe2, 0xd5, 0x00, 0x00, 0x00};
 
 extern void **_bios_txbuffer_p;
 
-void *udp_get_tx_buffer(void)
+void *xudp_get_tx_buffer(void)
 {
+  if (udp_get_tx_buffer)
+    return udp_get_tx_buffer();
+
   ethernet_buffer *txbuffer = *_bios_txbuffer_p;
   return txbuffer->frame.contents.udp.payload;
 }
@@ -30,7 +60,7 @@ int main()
   printf("Remote IP: %d.%d.%d.%d\n", REMOTEIP1, REMOTEIP2, REMOTEIP3, REMOTEIP4);
 
   ip = IPTOINT(REMOTEIP1, REMOTEIP2, REMOTEIP3, REMOTEIP4);
-  udp_start_constprop_0(IPTOINT(LOCALIP1, LOCALIP2, LOCALIP3, LOCALIP4), macadr);
+  xudp_start(macadr, IPTOINT(LOCALIP1, LOCALIP2, LOCALIP3, LOCALIP4));
 
   if(!udp_arp_resolve(ip)) {
     printf("arp resolve fail\n");
@@ -48,10 +78,10 @@ int main()
       while (*(volatile char *)CSR_AD9874IQ_READY_ADDR == 0) ;
       iqdat[j] = *(volatile int *)CSR_AD9874IQ_DATA_ADDR;
     }
-    buf = udp_get_tx_buffer();
+    buf = xudp_get_tx_buffer();
     for (j = 0; j < 256*4; j++)
       buf[j] = ((char *)iqdat)[j];
-    udp_send_constprop_0_isra_0(6000, 256*4);
+    xudp_send(7642, 6000, 256*4);
   }
 
   return 0;
